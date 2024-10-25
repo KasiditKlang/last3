2ef35186ea16c6ce22e4a74d7be7ff380f8f21f9
KasiditKlang <kasidit.ma@kkumail.com>
2024-10-25 13:13:44 +0700

    update

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve the data.json file
+app.get('/data.json', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'data.json'));
+});
+
+// Serve the HTML file
+app.get('*', (req, res) => {
+    res.sendFile('rndFood.html', {root: 'public'});
+  });
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server is running on port 3000, http://localhost:3000/');
+});
